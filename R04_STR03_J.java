b5c42afbabf0ff4a04838b70035e4b45076a758a
nelou584 <73256054+nelou584@users.noreply.github.com>
2020-10-21 17:51:22 -0400

    Create R04_STR03_J.java

---
+++ R04_STR03_J.java
@@ -0,0 +1,7 @@
+/* STR03-J. Do not encode noncharacter data as a string */
+
+BigInteger x = new BigInteger("530500452766");
+byte[] byteArray = x.toByteArray();
+String s = new String(byteArray);
+byteArray = s.getBytes();
+x = new BigInteger(byteArray);
