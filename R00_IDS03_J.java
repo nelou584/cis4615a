1983c80c61bd853d4c7def43d50492c64d8559c6
nelou584 <73256054+nelou584@users.noreply.github.com>
2020-10-21 17:44:03 -0400

    Update R00_IDS03_J.java
    Added and implemented sanitizeUser function to prevent erroneous logs

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,7 +1,12 @@
 //IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
 
 if (login Successful){
-  logger.severe("User login succeeded for: " + username};
+  logger.severe("User login succeeded for: " + sanitizeUser(username)};
   } else {
-    logger.severe("User login failed for: " + username};
+    logger.severe("User login failed for: " + sanitizeUser(username)};
+}
+
+public String sanitizeUser(String username){
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+    ? username : "unauthorized user";
 }
