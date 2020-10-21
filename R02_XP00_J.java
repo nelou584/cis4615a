1db7db08c828e5bdf6e7d1c783208924b55f1c77
nelou584 <73256054+nelou584@users.noreply.github.com>
2020-10-21 18:04:37 -0400

    Update R02_XP00_J.java
    Checks return of delete() method, handles errors

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,7 +2,11 @@
 below: */
 
 public void deleteFile(){
-File someFile = new File("someFileName.txt");
-// Do something with someFile
-someFile.delete( );
+ 
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
+ 
 }
