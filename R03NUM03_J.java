95160f8298071eee46e01f6cc9209ca5c859cb7d
nelou584 <73256054+nelou584@users.noreply.github.com>
2020-10-21 17:48:24 -0400

    Create R03NUM03_J.java

---
+++ R03NUM03_J.java
@@ -0,0 +1,6 @@
+/*NUM03-J. Use integer types that can fully represent the possible range of unsigned data
+Given the non-compliant code below: */
+
+public static int getInteger(DataInputStream is) throws IO Exception {
+	return is.readInt();
+}
