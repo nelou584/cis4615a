8c113239088ea46f2b3f269d4bc26a647d52cd01
nelou584 <73256054+nelou584@users.noreply.github.com>
2020-10-21 18:06:21 -0400

    Update R03_NUM03_J.java
    Mask upper bits of long in order to limit range of 32 bit integer

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,6 +1,6 @@
-/*NUM03-J. Use integer types that can fully represent the possible range of unsigned data
-Given the non-compliant code below: */
+/*NUM03-J. Use integer types that can fully represent the possible range of unsigned data */
 
-public static int getInteger(DataInputStream is) throws IO Exception {
-	return is.readInt();
+
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
